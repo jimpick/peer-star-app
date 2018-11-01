b83a11f6f38ea844f5fa6c3baa0ccbbd0c0ec576
Jim Pick <jim@backblend.com>
2018-10-31 22:24:25 -0700

    Use @jimpick/delta-crdts with fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 exports = module.exports = require('./app')
 exports.keys = require('./keys')
 exports.generateRandomName = require('./keys/generate-random-name')
-exports.collaborationTypes = require('delta-crdts')
+exports.collaborationTypes = require('@jimpick/delta-crdts')
 exports.debug = require('debug')
 exports.isCollaboration = require('./collaboration/is-collaboration')
